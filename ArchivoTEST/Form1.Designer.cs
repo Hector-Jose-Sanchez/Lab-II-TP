869634ff86d18cd061ac2d0e6e333bddac262d9d
Hector-Jose-Sanchez <141934356+Hector-Jose-Sanchez@users.noreply.github.com>
2024-07-12 10:17:31 -0300

    Cantidad de palabras

---
+++ ArchivoTEST/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ArchivoTEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pizarra = new System.Windows.Forms.TextBox();
+            this.Panel = new System.Windows.Forms.TextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonSee = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Pizarra
+            // 
+            this.Pizarra.Location = new System.Drawing.Point(43, 70);
+            this.Pizarra.Multiline = true;
+            this.Pizarra.Name = "Pizarra";
+            this.Pizarra.Size = new System.Drawing.Size(354, 289);
+            this.Pizarra.TabIndex = 0;
+            // 
+            // Panel
+            // 
+            this.Panel.Location = new System.Drawing.Point(450, 70);
+            this.Panel.Multiline = true;
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(227, 289);
+            this.Panel.TabIndex = 1;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(43, 32);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 32);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "Cargar";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(192, 32);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 32);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Gurdar";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonSee
+            // 
+            this.buttonSee.Location = new System.Drawing.Point(522, 32);
+            this.buttonSee.Name = "buttonSee";
+            this.buttonSee.Size = new System.Drawing.Size(75, 32);
+            this.buttonSee.TabIndex = 4;
+            this.buttonSee.Text = "Mostrar";
+            this.buttonSee.UseVisualStyleBackColor = true;
+            this.buttonSee.Click += new System.EventHandler(this.buttonSee_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(322, 32);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 32);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Limpiar";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pizarra);
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSee);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Pizarra;
+        private System.Windows.Forms.TextBox Panel;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonSee;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ ArchivoTEST/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArchivoTEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName)) {
+                    sw.WriteLine(Pizarra.Text);
+                }
+            }
+
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                {
+                    Pizarra.Text=sr.ReadToEnd();
+                }
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Pizarra.Text = "";
+        }
+
+        private void buttonSee_Click(object sender, EventArgs e)
+        {
+            string Text = Pizarra.Text;
+
+            // Cantidad de caracteres con espacios
+            int cones = Text.Length;
+
+            // Cantidad de caracteres sin espacios
+            int sines = Text.Replace(" ", "").Length;
+
+            // Cantidad de palabras
+            string[] palablas = Text.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int total = palablas.Length;
+
+            string result = $"Cantidad de caracteres (con espacios): {cones}\r\n";
+            result += $"Cantidad de caracteres (sin espacios): {sines}\r\n";
+            result += $"Cantidad de palabras: {total}\r\n";
+            result += $"Palabras más repetidas:\r\n";
+
+            // Contar la frecuencia de cada palabra
+            Dictionary<string, int> palafre = new Dictionary<string, int>();
+            foreach (string palabra in palablas)
+            {
+                if (palafre.ContainsKey(palabra))
+                {
+                    palafre[palabra]++;
+                }
+                else
+                {
+                    palafre[palabra] = 1;
+                }
+            }
+
+            // Obtener las 10 palabras más repetidas
+            var topala = palafre.OrderByDescending(w => w.Value).Take(5).ToList();
+            int rank = 1;
+            foreach (var palabra in topala)
+            {
+                result += $"{rank}. {palabra.Key} - {palabra.Value} veces, {palabra.Key.Length} caracteres\r\n";
+                rank++;
+            }
+
+            Panel.Text = result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
